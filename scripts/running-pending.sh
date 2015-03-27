538fee75507ea6d8c54d74bcd50c40bcb95a98fe
root <root@host18-rack11.scale.openstack.engineering.redhat.com>
2015-03-27 19:23:13 +0000

    running/pending

--- scripts/running-pending.sh
+++ scripts/running-pending.sh
@@ -3,8 +3,8 @@ while true;
 do
    ddd=`date +%H:%M:%S,%s` ;
    raw=`kubectl get pods` ;
-   running=`echo $raw | grep unning | wc -l` ;
-   pending=`echo $raw | grep ending | wc -l` ;
+   pending=$(echo "$raw" | grep ending | wc -l) 
+   running=$(echo "$raw" | grep unning | wc -l) 
    echo "$ddd,$running,$pending" ;
    sleep 1;
 done
