5e1a2957e3344d381a68e184346f67b3ffe1b152
Robert Rati <rrati@redhat.com>
2015-07-09 14:36:41 -0400

    Updating scripts for ha cluster configuration

--- scripts/running-pending.sh
+++ scripts/running-pending.sh
@@ -2,9 +2,10 @@
 while true; 
 do
    ddd=`date +%H:%M:%S,%s` ;
-   raw=`kubectl get pods` ;
-   pending=$(echo "$raw" | grep ending | grep host | wc -l) 
-   running=$(echo "$raw" | grep unning | grep host | wc -l) 
-   echo "$ddd,$running,$pending" ;
+   raw=`kubectl -s http://kube-apiserver.scale.openstack.engineering.redhat.com:8080 get pods` ;
+   pending=$(echo "$raw" | grep ending | wc -l) 
+   running=$(echo "$raw" | grep unning | wc -l) 
+   waiting=$(echo "$raw" | grep aiting | wc -l) 
+   echo "$ddd,$running,$pending,$waiting" ;
    sleep 1;
 done
